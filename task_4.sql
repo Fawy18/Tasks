222b896fa468f6249ad538f0bc7b7cb74326c101
Lavrinenko Vladyslav <73023725+Fawy18@users.noreply.github.com>
2020-12-01 19:33:48 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,29 +1,29 @@
 CREATE DATABASE sql_task4;
 
 CREATE TABLE course(
-        id SMALLINT NOT NULL AUTO_INCREMENT,
-        course_title VARCHAR(30),
-        PRIMARY KEY(id)
+    id SMALLINT NOT NULL AUTO_INCREMENT,
+    course_title VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE student(
     id SMALLINT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(20),
-        last_name VARCHAR(20),
-        id_groups int,
+    last_name VARCHAR(20),
+    id_groups int,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE teacher(
     id SMALLINT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(20),
-        last_name VARCHAR(20),
-        email VARCHAR(40),
+    last_name VARCHAR(20),
+    email VARCHAR(40),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE marks(
-id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     student_id SMALLINT NOT NULL ,
     course_id SMALLINT NOT NULL,
     mark TINYINT,
@@ -38,14 +38,14 @@ CREATE TABLE groups(
 );
 
 CREATE TABLE help_table(
-course_id SMALLINT NOT NULL AUTO_INCREMENT,
-groups_id SMALLINT NOT NULL AUTO_INCREMENT,
+    course_id SMALLINT NOT NULL AUTO_INCREMENT,
+    groups_id SMALLINT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE help_table_2(
-teacher_id SMALLINT NOT NULL AUTO_INCREMENT,
-course_id SMALLINT NOT NULL AUTO_INCREMENT,
+    teacher_id SMALLINT NOT NULL AUTO_INCREMENT,
+    course_id SMALLINT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id)
 );
 -- 1 задание
@@ -57,7 +57,7 @@ JOIN course ON course.id = help_table_2.course_id
 JOIN help_table ON  help_table.course_id = course.id 
 JOIN group ON groups.id = help_table.groups_id 
 JOIN student ON student.id_groups = groups.id
-group by student_id
+group by course_id
 having count(student_id) > 2000;
 
 -- 2 задание
@@ -73,7 +73,8 @@ JOIN marks ON student.id = marks.student_id
 JOIN marks ON course.cource_id = marks.cource_id
 where marks.course_title - 'Programming'
 group by student_id
-having avg(mark) > 4;
+having avg(mark) > 4
+order by ASK(last_name);
 
 -- 3 задание
 
